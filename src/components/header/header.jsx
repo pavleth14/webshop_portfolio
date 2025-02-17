432d99a1495e67db6697eadd01856bb65906c6ae
Pavle Stojanovic <pavleth14@hotmail.com>
2025-02-17 13:19:54 +0100

    hamburger working

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,28 +1,54 @@
 import { Link, useLocation } from 'react-router-dom'; // Import za ruter
 import { useSelector } from 'react-redux';
-import './header.css'
-
+import './header.css';
+import { FaBars } from 'react-icons/fa'; // Import hamburger ikone
+import { useState } from 'react';
 
 const Header = () => {
-
     const totalItems = useSelector(state => state.shop.allItems);
     const location = useLocation();
+    const [isFaBarsClicked, setIsFaBarsClicked] = useState(false);
+
+    const handleFaBars = () => {
+        setIsFaBarsClicked(prev => !prev);
+    }
 
     return (
-        <div className='headerDiv'>
-            <h1>Header</h1>
-            <div className='rightSideHeader'>
-                <ul>
-                    <Link to={'/'}>Home</Link>
-                    <Link to={'/about'}>About</Link>
-                    <Link to={'/contact'}>Contact</Link>
-                </ul>                
+        <div>
+            <div className='headerDiv'>
+                <h1>Header</h1>
+                <div className='rightSideHeader'>
+                    {/* Standardni linkovi za veće ekrane */}
+                    <div className='headerLinks'>
+                        <ul>
+                            <li><Link to={'/'}>Home</Link></li>
+                            <li><Link to={'/about'}>About</Link></li>
+                            <li><Link to={'/contact'}>Contact</Link></li>
+                        </ul>
+                    </div>
+
+                    {/* Hamburger ikona */}
+                    <div className="hamburger">
+                        <FaBars onClick={handleFaBars} className="hamburger-icon" size={30} />
+                    </div>
+
+                    {/* Prikazivanje broja artikala u korpi */}
                     {location.pathname === '/cart' ? (
                         <Link to="/">Go Home</Link>
                     ) : (
                         <Link to="/cart">Go to Cart: {totalItems.length}</Link>
                     )}
+                </div>
             </div>
+
+            {/* Meni sa linkovima koji se prikazuje kada je hamburger ikona kliknuta */}
+            {isFaBarsClicked && (
+                <ul className='mobileBars'>
+                    <li><Link to={'/'}>Home</Link></li>
+                    <li><Link to={'/about'}>About</Link></li>
+                    <li><Link to={'/contact'}>Contact</Link></li>
+                </ul>
+            )}
         </div>
     );
 }
